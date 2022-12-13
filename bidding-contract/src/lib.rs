74b30ea36356296796679a7aaa4b430f240a2c88
Bartłomiej Kuras <bart.k@confio.gmbh>
2022-12-13 22:06:20 +0100

    Contract wireframe

---
+++ bidding-contract/src/lib.rs
@@ -0,0 +1,44 @@
+use cosmwasm_schema::cw_serde;
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
+use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+
+#[cw_serde]
+pub struct InstantiateMsg {}
+
+#[cw_serde]
+pub enum ExecuteMsg {}
+
+#[cw_serde]
+pub enum QueryMsg {}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    _deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: InstantiateMsg,
+) -> StdResult<Response> {
+    Ok(Response::default())
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(
+    _deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: ExecuteMsg,
+) -> StdResult<Response> {
+    Ok(Response::default())
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(
+    _deps: Deps,
+    _env: Env,
+    _msg: QueryMsg,
+) -> StdResult<Binary> {
+    Ok(Binary::default())
+}
+
+
